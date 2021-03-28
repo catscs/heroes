ce1fbf56cf6e99da286f73b8249f3e5550b9cde1
Félix Luján <felix.lujan@recordrentacar.com>
2021-03-28 18:08:46 +0200

    Create service menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from '@dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
   {
     path: 'heroes',
     loadChildren: () =>
       import('./heroes/heroes.module').then((m) => m.HeroesModule),
   },
-  { path: '**', redirectTo: '' },
+  { path: '**', redirectTo: 'dashboard' },
 ];
 
 @NgModule({
